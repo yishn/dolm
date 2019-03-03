9fe59264a061d1350d06c010b0483a71d374e926
Yichuan Shen <shen.yichuan@gmail.com>
2019-03-03 22:02:13 +0100

    Pass local dolm object to translation functions

--- src/main.js
+++ src/main.js
@@ -5,13 +5,19 @@ exports.load = strings => ({
         let key = typeof input !== 'function'
             ? input
             : input(
-                Object.keys(args)
-                .reduce((acc, key) => (acc[key] = `\${${key}}`, acc), {})
+                // Build dummy args object
+                Object.keys(args).reduce((acc, key) => (
+                    acc[key] = `\${${key}}`,
+                    acc
+                ), {}),
+                this
             )
 
         let value = strings[key] || input
         this.usedStrings[key] = !strings[key] ? null : value
 
-        return typeof value === 'function' ? value(args) : value
+        return typeof value !== 'function'
+            ? value
+            : value(args, this)
     }
 })
