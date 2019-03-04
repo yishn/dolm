94f498d5276c050386f7cafc8857d843d687ef45
Yichuan Shen <shen.yichuan@gmail.com>
2019-03-04 19:54:01 +0100

    Convert 4-spaces indentation to 2-spaces

--- src/main.js
+++ src/main.js
@@ -1,36 +1,36 @@
 exports.load = strings => {
-    let usedStrings = {}
+  let usedStrings = {}
 
-    return {
+  return {
+    usedStrings,
+
+    context(context) {
+      usedStrings[context] = {}
+
+      return {
+        context,
         usedStrings,
 
-        context(context) {
-            usedStrings[context] = {}
+        t(input, args = {}) {
+          let key = typeof input !== 'function'
+            ? input
+            : input(
+              // Build dummy args object
+              Object.keys(args).reduce((acc, key) => (
+                acc[key] = `\${${key}}`,
+                acc
+              ), {}),
+              this
+            )
 
-            return {
-                context,
-                usedStrings,
+          let value = strings[key] || input
+          usedStrings[context][key] = !strings[key] ? null : value
 
-                t(input, args = {}) {
-                    let key = typeof input !== 'function'
-                        ? input
-                        : input(
-                            // Build dummy args object
-                            Object.keys(args).reduce((acc, key) => (
-                                acc[key] = `\${${key}}`,
-                                acc
-                            ), {}),
-                            this
-                        )
-
-                    let value = strings[key] || input
-                    usedStrings[context][key] = !strings[key] ? null : value
-
-                    return typeof value !== 'function'
-                        ? value
-                        : value(args, this)
-                }
-            }
+          return typeof value !== 'function'
+            ? value
+            : value(args, this)
         }
+      }
     }
+  }
 }
--- src/util.js
+++ src/util.js
@@ -1,21 +1,21 @@
 exports.serialize = (obj, indent = '  ') => {
-    if (!obj)
-        return 'null'
-    else if (typeof obj === 'function')
-        return obj.toString()
-    else if (typeof obj !== 'object')
-        return JSON.stringify(`${obj}`)
+  if (!obj)
+    return 'null'
+  else if (typeof obj === 'function')
+    return obj.toString()
+  else if (typeof obj !== 'object')
+    return JSON.stringify(`${obj}`)
 
-    return [
-        '{',
-        Object.keys(obj).sort().map(key => {
-            let value = exports.serialize(obj[key])
-                .split('\n')
-                .map((line, i) => i === 0 ? line : `${indent}${line}`)
-                .join('\n')
+  return [
+    '{',
+    Object.keys(obj).sort().map(key => {
+      let value = exports.serialize(obj[key])
+        .split('\n')
+        .map((line, i) => i === 0 ? line : `${indent}${line}`)
+        .join('\n')
 
-            return `${indent}${JSON.stringify(key)}: ${value},`
-        }).join('\n'),
-        '}'
-    ].join('\n')
+      return `${indent}${JSON.stringify(key)}: ${value},`
+    }).join('\n'),
+    '}'
+  ].join('\n')
 }
