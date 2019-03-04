ff29fc90eff6e319f60f62d08f507fa5af704072
Yichuan Shen <shen.yichuan@gmail.com>
2019-03-04 21:09:35 +0100

    Add more tests

--- tests/data.js
+++ tests/data.js
@@ -1,15 +1,15 @@
 const dolm = require('..')
 
 let strings = {
-    general: {
-        'Hello World!': 'Hallo Welt!',
-        'apples': p => `${p.count === 1 ? 'Apfel' : 'Äpfel'}`,
-        'I have ${count} apples': (p, t) => `Ich habe ${p.count} ${t('apples', p)}`,
-    },
-    special: {
-        'Edit on GitHub': 'Auf GitHub bearbeiten',
-        'My name is ${name}': p => `Mein Name ist ${p.name}`,
-    },
+  general: {
+    'Hello World!': 'Hallo Welt!',
+    'apples': p => `${p.count === 1 ? 'Apfel' : 'Äpfel'}`,
+    'I have ${count} apples': (p, t) => `Ich habe ${p.count} ${t('apples', p)}`
+  },
+  special: {
+    'Edit on GitHub': 'Auf GitHub bearbeiten',
+    'My name is ${name}': p => `Mein Name ist ${p.name}`
+  }
 }
 
 module.exports = dolm.load(strings)
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,28 +1,53 @@
 const tap = require('tap')
 const {context, serialize} = require('./data')
 
-tap.test('get non-existent context', tap => {
-    let {t} = context('non-existent')
+async function test() {
+  await Promise.all([
+    tap.test('get non-existent context', async tap => {
+      let {t} = context('non-existent')
 
-    tap.equal(t('Hello World!'), 'Hello World!')
-    tap.equal(t(p => `Hello ${p.name}`, {name: 'Yichuan'}), 'Hello Yichuan')
-    tap.end()
-})
+      tap.equal(t('Hello World!'), 'Hello World!')
+      tap.equal(t(p => `Hello ${p.name}`, {name: 'Yichuan'}), 'Hello Yichuan')
+    }),
 
-tap.test('get non-existent strings in existing context', tap => {
-    let {t} = context('special')
+    tap.test('get non-existent strings in existing context', async tap => {
+      let {t} = context('special')
 
-    tap.equal(t('Hello World!'), 'Hello World!')
-    tap.equal(t(p => `Hello ${p.name}`, {name: 'Yichuan'}), 'Hello Yichuan')
-    tap.end()
-})
+      tap.equal(t('Hello World!'), 'Hello World!')
+      tap.equal(t(p => `Hello ${p.name}`, {name: 'Yichuan'}), 'Hello Yichuan')
+    }),
 
-tap.test('get existing strings', tap => {
-    let {t} = context('general')
+    tap.test('get existing strings', async tap => {
+      let {t} = context('general')
 
-    tap.equal(t('Hello World!'), 'Hallo Welt!')
-    tap.equal(t((p, t) => `I have ${p.count} ${t('apples', p)}`, {count: 0}), 'Ich habe 0 Äpfel')
-    tap.equal(t((p, t) => `I have ${p.count} ${t('apples', p)}`, {count: 1}), 'Ich habe 1 Apfel')
-    tap.equal(t((p, t) => `I have ${p.count} ${t('apples', p)}`, {count: 2}), 'Ich habe 2 Äpfel')
-    tap.end()
-})
+      tap.equal(t('Hello World!'), 'Hallo Welt!')
+      tap.equal(t((p, t) => `I have ${p.count} ${t('apples', p)}`, {count: 0}), 'Ich habe 0 Äpfel')
+      tap.equal(t((p, t) => `I have ${p.count} ${t('apples', p)}`, {count: 1}), 'Ich habe 1 Apfel')
+      tap.equal(t((p, t) => `I have ${p.count} ${t('apples', p)}`, {count: 2}), 'Ich habe 2 Äpfel')
+    })
+  ])
+
+  tap.test('serialize', async tap => {
+    let usedStrings = serialize()
+
+    tap.equal(usedStrings, `
+{
+  "general": {
+    "Hello World!": "Hallo Welt!",
+    "I have \${count} apples": (p, t) => \`Ich habe \${p.count} \${t('apples', p)}\`,
+    "apples": p => \`\${p.count === 1 ? 'Apfel' : 'Äpfel'}\`,
+  },
+  "non-existent": {
+    "Hello \${name}": null,
+    "Hello World!": null,
+  },
+  "special": {
+    "Hello \${name}": null,
+    "Hello World!": null,
+  },
+}
+    `.trim())
+  })
+}
+
+test()
