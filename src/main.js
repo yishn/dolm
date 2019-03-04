0a7da7d7de96b4552c5988b31ab773ccb48b6d1b
Yichuan Shen <shen.yichuan@gmail.com>
2019-03-04 20:04:28 +0100

    Move serialize() into main.js

--- src/main.js
+++ src/main.js
@@ -31,6 +31,32 @@ exports.load = strings => {
             : value(args, this)
         }
       }
+    },
+
+    serialize(indent = '  ') {
+      let inner = obj => {
+        if (!obj)
+          return 'null'
+        else if (typeof obj === 'function')
+          return obj.toString()
+        else if (typeof obj !== 'object')
+          return JSON.stringify(`${obj}`)
+
+        return [
+          '{',
+          Object.keys(obj).sort().map(key => {
+            let value = inner(obj[key])
+              .split('\n')
+              .map((line, i) => i === 0 ? line : `${indent}${line}`)
+              .join('\n')
+
+            return `${indent}${JSON.stringify(key)}: ${value},`
+          }).join('\n'),
+          '}'
+        ].join('\n')
+      }
+
+      return inner(usedStrings)
     }
   }
 }
