966bc957aec24cd7685d9cf9f17f99ed73e7c319
Yichuan Shen <shen.yichuan@gmail.com>
2020-02-09 22:12:40 +0100

    Refactor getKey into own function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+exports.getKey = (input, params = {}) => {
+  return typeof input !== 'function'
+    ? input.toString()
+    : input(
+        // Build dummy params object
+        Object.keys(params).reduce(
+          (acc, key) => ((acc[key] = `\${${key}}`), acc),
+          {}
+        )
+      )
+}
+
 exports.load = strings => {
   let instance = {
     strings,
@@ -12,17 +24,7 @@ exports.load = strings => {
 
     context(context) {
       return (input, params = {}) => {
-        let key =
-          typeof input !== 'function'
-            ? input
-            : input(
-                // Build dummy params object
-                Object.keys(params).reduce(
-                  (acc, key) => ((acc[key] = `\${${key}}`), acc),
-                  {}
-                )
-              )
-
+        let key = exports.getKey(input, params)
         let value = (strings[context] || {})[key] || input
 
         return typeof value !== 'function' ? value : value(params)
