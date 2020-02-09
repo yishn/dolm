4f7781c39f4ce0d018a95b6f3b253145662dc9b5
Yichuan Shen <shen.yichuan@gmail.com>
2020-02-09 21:14:55 +0100

    Remove .serialize() from main

--- src/main.js
+++ src/main.js
@@ -37,68 +37,6 @@ exports.load = strings => {
 
         return typeof value !== 'function' ? value : value(params)
       }
-    },
-
-    serialize(indent = '  ') {
-      let translatedCount = 0
-      let untranslatedCount = 0
-
-      let inner = (obj, path = []) => {
-        if (!obj) {
-          untranslatedCount++
-          return 'null'
-        } else if (typeof obj === 'function') {
-          translatedCount++
-          return obj.toString()
-        } else if (typeof obj !== 'object') {
-          translatedCount++
-          return JSON.stringify(`${obj}`)
-        }
-
-        return [
-          '{',
-          Object.keys(obj)
-            .sort()
-            .map(key => {
-              let lines = inner(obj[key], [...path, key]).split('\n')
-              let unused = !(path.reduce(
-                (acc, key) => acc && acc[key],
-                usedStrings
-              ) || {})[key]
-
-              let slice = Math.min(
-                ...lines.map((x, i) =>
-                  i === 0 ? Infinity : x.match(/^\s*/)[0].length
-                )
-              )
-
-              let value = lines
-                .map((line, i) =>
-                  i === 0 ? line : `${indent + indent}${line.slice(slice)}`
-                )
-                .join('\n')
-
-              return `${indent}${unused ? '/* unused */ ' : ''}${JSON.stringify(
-                key
-              )}: ${value},`
-            })
-            .join('\n'),
-          '}'
-        ].join('\n')
-      }
-
-      let js = inner(strings)
-      let complete =
-        untranslatedCount + translatedCount === 0
-          ? 0
-          : translatedCount / (untranslatedCount + translatedCount)
-
-      return {
-        translatedCount,
-        untranslatedCount,
-        complete,
-        js
-      }
     }
   }
 
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,71 +1,55 @@
 const tap = require('tap')
 const dolm = require('./data')
 
-async function test() {
-  await Promise.all([
-    tap.test('get non-existent context', async tap => {
-      let t = dolm.context('non-existent')
+tap.test('get non-existent context', async tap => {
+  let t = dolm.context('non-existent')
 
-      tap.strictEqual(t('Hello World!'), 'Hello World!')
-      tap.strictEqual(
-        t(p => `Hello ${p.name}`, {name: 'Yichuan'}),
-        'Hello Yichuan'
-      )
-    }),
+  tap.strictEqual(t('Hello World!'), 'Hello World!')
+  tap.strictEqual(
+    t(p => `Hello ${p.name}`, {name: 'Yichuan'}),
+    'Hello Yichuan'
+  )
+})
 
-    tap.test('get non-existent strings in existing context', async tap => {
-      let t = dolm.context('special')
+tap.test('get non-existent strings in existing context', async tap => {
+  let t = dolm.context('special')
 
-      tap.strictEqual(t('Hello World!'), 'Hello World!')
-      tap.strictEqual(
-        t(p => `Hello ${p.name}`, {name: 'Yichuan'}),
-        'Hello Yichuan'
-      )
-    }),
+  tap.strictEqual(t('Hello World!'), 'Hello World!')
+  tap.strictEqual(
+    t(p => `Hello ${p.name}`, {name: 'Yichuan'}),
+    'Hello Yichuan'
+  )
+})
 
-    tap.test('get existing strings', async tap => {
-      let t = dolm.context('general')
+tap.test('get existing strings', async tap => {
+  let t = dolm.context('general')
 
-      tap.strictEqual(t('Hello World!'), 'Hallo Welt!')
+  tap.strictEqual(t('Hello World!'), 'Hallo Welt!')
 
-      let complexString = p =>
-        `I have ${['no', 'one'][p.count] || p.count} apple${p !== 1 ? 's' : ''}`
+  let complexString = p =>
+    `I have ${['no', 'one'][p.count] || p.count} apple${p !== 1 ? 's' : ''}`
 
-      tap.strictEqual(t(complexString, {count: 0}), 'Ich habe keine Äpfel')
-      tap.strictEqual(t(complexString, {count: 1}), 'Ich habe einen Apfel')
-      tap.strictEqual(t(complexString, {count: 2}), 'Ich habe 2 Äpfel')
-    }),
+  tap.strictEqual(t(complexString, {count: 0}), 'Ich habe keine Äpfel')
+  tap.strictEqual(t(complexString, {count: 1}), 'Ich habe einen Apfel')
+  tap.strictEqual(t(complexString, {count: 2}), 'Ich habe 2 Äpfel')
+})
 
-    tap.test('get existing strings directly', async tap => {
-      tap.strictEqual(dolm.t('general', 'Hello World!'), 'Hallo Welt!')
+tap.test('get existing strings directly', async tap => {
+  tap.strictEqual(dolm.t('general', 'Hello World!'), 'Hallo Welt!')
 
-      let complexString = p =>
-        `I have ${['no', 'one'][p.count] || p.count} apple${p !== 1 ? 's' : ''}`
+  let complexString = p =>
+    `I have ${['no', 'one'][p.count] || p.count} apple${p !== 1 ? 's' : ''}`
 
-      tap.strictEqual(
-        dolm.t('general', complexString, {count: 0}),
-        'Ich habe keine Äpfel'
-      )
-      tap.strictEqual(
-        dolm.t('general', complexString, {count: 1}),
-        'Ich habe einen Apfel'
-      )
-      tap.strictEqual(
-        dolm.t('general', complexString, {count: 2}),
-        'Ich habe 2 Äpfel'
-      )
-    })
-  ])
-
-  tap.test('serialize', async tap => {
-    let serialized = dolm.serialize()
-
-    tap.strictEqual(serialized.translatedCount, 4)
-    tap.strictEqual(serialized.untranslatedCount, 4)
-    tap.strictEqual(serialized.complete, 0.5)
-
-    tap.matchSnapshot(serialized.js.replace(/\r/g, ''))
-  })
-}
-
-test()
+  tap.strictEqual(
+    dolm.t('general', complexString, {count: 0}),
+    'Ich habe keine Äpfel'
+  )
+  tap.strictEqual(
+    dolm.t('general', complexString, {count: 1}),
+    'Ich habe einen Apfel'
+  )
+  tap.strictEqual(
+    dolm.t('general', complexString, {count: 2}),
+    'Ich habe 2 Äpfel'
+  )
+})
